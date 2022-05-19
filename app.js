39aec70e2a10a646488d28472d4060807a0cd1b8
saufiroja <saufiroja08@gmail.com>
2022-05-19 22:25:19 +0800

    test

--- app.js
+++ app.js
@@ -1,53 +1,55 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = process.env.port || 8000;
-const Sequelize = require('sequelize')
 
 const userController = require('./controllers/usercontroller');
 const method = require('./controllers/method');
+const { register } = require('./controllers/auth');
 
+// connection database
+require('./models/sequelize');
 
 // Setting template engine EJS
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-//set support body 
-app.use(express.urlencoded({
-    extended: false
-}));
+//set support body
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 app.use(express.json());
 
 //important to link css and other static file that in public folder
 app.use(express.static('public'));
 
-
-
 //INDEX OR START
-app.get('/', userController.homeRender)
+app.get('/', userController.homeRender);
 
 //GAMEPLAY
-app.get('/game', userController.gameRender)
-
+app.get('/game', userController.gameRender);
 
 //LOGIN PAGE will redirect /game/:username
-app.get('/login', userController.loginGet)
-app.post('/login', userController.loginPost)
-
+app.get('/login', userController.loginGet);
+app.post('/login', userController.loginPost);
 
 //REGISTER PAGE or Create Method here
-app.get('/register', userController.registerGet)
-app.post('/register', userController.registerPost)
+app.get('/register', userController.registerGet);
+app.post('/register', userController.registerPost);
 
 //Read Method here
-app.get('/user_list', method.user_list)
+app.get('/user_list', method.user_list);
 
 //Update Method here
-app.get('/user_update', method.updateGet)
-app.post('/user_update', method.updateInfo) 
+app.get('/user_update', method.updateGet);
+app.post('/user_update', method.updateInfo);
 
 //Delete Method here
-app.get('/user_delete', method.deleteGet)
-app.post('/user_delete', method.deleteDestroy)
+app.get('/user_delete', method.deleteGet);
+app.post('/user_delete', method.deleteDestroy);
+
+app.post('/signup', register);
 
 app.listen(port, () => {
-    console.log(`Go to http://localhost:${port}`)
-})
+  console.log(`Go to http://localhost:${port}`);
+});
